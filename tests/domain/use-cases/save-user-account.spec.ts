7da08af6102f50ce7c084c743ef55c4b143f537f
Marcos Soares <marcossaore@gmail.com>
2022-01-25 20:16:54 -0300

    ensure SaveUserAccount returns an account when succeds

--- tests/domain/use-cases/save-user-account.spec.ts
+++ tests/domain/use-cases/save-user-account.spec.ts
@@ -2,13 +2,14 @@ import { mock } from 'jest-mock-extended'
 
 class SaveUserAccount {
   constructor (private readonly saveUserAccountRepository: SaveUserAccountRepository) {}
-  async save (userAccount: any): Promise<void> {
-    await this.saveUserAccountRepository.saveUserAccount(userAccount)
+  async save (userAccount: any): Promise<any> {
+    const user = await this.saveUserAccountRepository.saveUserAccount(userAccount)
+    return user
   }
 }
 
 interface SaveUserAccountRepository {
-  saveUserAccount: (userAccount: any) => Promise<void>
+  saveUserAccount: (userAccount: any) => Promise<any>
 }
 
 describe('SaveUserAccount UseCase', () => {
@@ -26,7 +27,27 @@ describe('SaveUserAccount UseCase', () => {
     const sut = new SaveUserAccount(saveUserAccountRepositorySpy)
 
     await sut.save(userAccount)
+
     expect(saveUserAccountRepositorySpy.saveUserAccount).toHaveBeenCalledWith(userAccount)
     expect(saveUserAccountRepositorySpy.saveUserAccount).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return an user account when succeds', async () => {
+    const saveUserAccountRepositorySpy = mock<SaveUserAccountRepository>()
+    const sut = new SaveUserAccount(saveUserAccountRepositorySpy)
+    const userAccount = {
+      name: 'any_name',
+      email: 'any_email',
+      birthDate: 'any_birthDate',
+      phone: 'any_phone',
+      password: 'any_password',
+      cpf: 'any_cpf',
+      rg: 'any_rg'
+    }
+    saveUserAccountRepositorySpy.saveUserAccount.mockResolvedValueOnce(userAccount)
+
+    const user = await sut.save(userAccount)
+
+    expect(user).toBe(userAccount)
   })
 })
