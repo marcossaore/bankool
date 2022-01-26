0f3ceb4c9d7a24beb692a0960a7a683fb94e5050
Marcos Soares <marcossaore@gmail.com>
2022-01-25 23:00:26 -0300

    add Controller and your own validations

---
+++ src/application/validation/index.ts
@@ -0,0 +1,7 @@
+
+export * from './required'
+export * from './allowed-mime-types'
+export * from './max-file-size'
+export * from './validator'
+export * from './validator-composite'
+export * from './builder'
